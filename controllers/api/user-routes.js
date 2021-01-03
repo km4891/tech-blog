788910854d6f3dbf842df3c91f7dde0d4b5871fe
km4891 <kmorarity@gmail.com>
2021-01-02 19:45:55 -0800

    working on home-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -95,7 +95,49 @@ router.put('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
+//user login
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_text', 'created_at']
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that username' });
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
 
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
+});
+
+//user logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 //DELETE user by id
 router.delete('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const sess = {
     })
 };
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
